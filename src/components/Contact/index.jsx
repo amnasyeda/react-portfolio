030071d20c7e014e7ee717990508465028f22f30
Amna Syeda <amna.syeda98@gmail.com>
2021-09-13 12:03:48 -0400

    contact + footer css

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,6 +2,10 @@ const Contact = () => {
   return (
     <div className="contact">
       <h1>Contact me!</h1>
+      <img
+              src="https://raw.githubusercontent.com/amnasyeda/react-portfolio/main/public/css/contact.png"
+              alt="contact"
+            />
       <iframe
         src=""
         frameBorder="0"
@@ -12,6 +16,7 @@ const Contact = () => {
 
       <h4>Email</h4>
       <p>amna.syeda98@gmail.com</p>
+
     </div>
   );
 };
