87b6dbfc3584a70d9226ff4a8cd6df716541fc33
Amna Syeda <amna.syeda98@gmail.com>
2021-09-13 10:26:26 -0400

    footer and contact page

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,20 @@
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>Contact me!</h1>
+      <iframe
+        src=""
+        frameBorder="0"
+        allowFullScreen=""
+        aria-hidden="false"
+        tabIndex="0"
+      ></iframe>
+
+      <h4>Email</h4>
+      <p>amna.syeda98@gmail.com</p>
+    </div>
+  );
+};
+
+
+export default Contact;
