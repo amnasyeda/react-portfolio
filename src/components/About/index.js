a89bbb7075be085dbfee72f383dc1b346d05c156
Amna Syeda <amna.syeda98@gmail.com>
2021-09-13 10:02:30 -0400

    formatting was not responsive, renaming files and redoing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,10 @@
-import React from "react";
+import './style.css'
 
-function About () {
-    return(
-        <> 
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Me!</h1>
 
-        <div id="mywork" class="search">
-                <h2 class="headertwo">My Background</h2>
                 <p>
                     I am a recent graduate from the University of Toronto and completed my Bachelors of Science in Human Biology and Neuroscience. 
                     I am now a research assistant for a professor at the University of Toronto Scarborough campus and I am studying infant development,
@@ -15,8 +14,6 @@ function About () {
                     becoming a full stack web developer. 
           </p>
          </div>
-        </>
     )
 }
-
-export default About;
+export default About
