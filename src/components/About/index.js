87a5fe992f97d424aaea2f279b0408828769a6b4
Amna Syeda <amna.syeda98@gmail.com>
2021-09-13 12:56:16 -0400

    fix merge

---
+++ src/components/About/index.js
@@ -0,0 +1,23 @@
+import './style.css'
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Me!</h1>
+
+                <p>
+                    I am a recent graduate from the University of Toronto and completed my Bachelors of Science in Human Biology and Neuroscience. 
+                    I am now a research assistant for a professor at the University of Toronto Scarborough campus and I am studying infant development,
+                    specifically rhythm perception across infancy and childhood. 
+                    
+                    Over the last few months I began a coding bootcamp at the University of Toronto School of Continuing Studies and I am working towards 
+                    becoming a full stack web developer. 
+          </p>
+          <img
+              src="https://raw.githubusercontent.com/amnasyeda/react-portfolio/main/public/css/brain.jpeg"
+              alt="brain"
+            />
+         </div>
+    )
+}
+export default About
